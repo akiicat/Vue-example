ec6818d22d974c4c3fe7e9421827acc501ef4228
akiicat <aaaa1379@gmail.com>
2017-06-13 22:50:28 +0800

    add f2c

--- src/components/C2F.vue
+++ src/components/C2F.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="container">
-    <h2>華氏：{{ celsius * 9 / 5 + 32 }} °F</h2>
-    <h2>華氏：{{ fahrenheit }} °F</h2>
-    <div class="celsius">
-      攝氏：<input type='number' v-model='celsius'/> °C
+    <h1>Temperature Conversion</h1>
+
+    <input id="toggleTemperature" type="checkbox" v-model="isCelsius"/>
+    <label for="toggleTemperature">切換：{{ toggleTemperature }}</label>
+
+    <h2 v-if="!isCelsius">攝氏：{{ fahrenheit }} °F</h2>
+    <h2 v-if="isCelsius">華氏：{{ celsius }} °C</h2>
+
+    <div class="userInput">
+      <span v-show="isCelsius">攝氏：</span>
+      <span v-show="!isCelsius">華氏：</span>
+      <input type='number' v-model='userInput'/>°C
     </div>
+
+    <button @click="temperatureConversio(userInput)">
+      Temperature Conversio
+    </button>
   </div>
 </template>
 
@@ -12,19 +24,35 @@
 export default {
   data () {
     return {
-      celsius: 0
+      userInput: 0,
+      isCelsius: false
     }
   },
   computed: {
     fahrenheit () {
-      return this.celsius * 9 / 5 + 32
+      return this.userInput * 9 / 5 + 32
+    },
+    celsius () {
+      return (this.userInput - 32) * 5 / 9
+    },
+    toggleTemperature () {
+      return this.isCelsius ? '攝氏 轉換 華氏' : '華氏 轉換 攝氏'
+    }
+  },
+  methods: {
+    temperatureConversio (val) {
+      if (this.isCelsius) {
+        alert('攝氏：' + this.celsius)
+      } else {
+        alert('攝氏：' + this.fahrenheit)
+      }
     }
   }
 }
 </script>
 
 <style>
-.celsius {
+.userInput {
   font-size: 1.5em;
 }
 </style>
